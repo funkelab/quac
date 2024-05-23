fix: :bug: Add normalization assumption in classifier

src/quac/generate/__init__.py
@@ -7,6 +7,7 @@ import logging
 from quac.training.classification import ClassifierWrapper
 import torch
 from torchvision import transforms
+from typing import Union
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
@@ -16,7 +17,9 @@ class CounterfactualNotFound(Exception):
     pass
 
 
-def load_classifier(checkpoint, mean=0.5, std=0.5, eval=True, device=None):
+def load_classifier(
+    checkpoint, mean=0.5, std=0.5, eval=True, assume_normalized=False, device=None
+):
     """
     Load a classifier from a torchscript checkpoint.
 
@@ -81,6 +84,7 @@ def load_stargan(
     checkpoint_iter: int = 100000,
     kind="latent",
     single_output_encoder: bool = False,
+    final_activation: Union[str, None] = None,
 ) -> torch.nn.Module:
     """
     Load an inference version of the StarGANv2 model from a checkpoint.
@@ -108,6 +112,7 @@ def load_stargan(
             latent_dim=latent_dim,
             num_domains=num_domains,
             single_output_encoder=single_output_encoder,
+            final_activation=final_activation,
         )
     else:
         latent_inference_model = LatentInferenceModel(
@@ -117,6 +122,7 @@ def load_stargan(
             style_dim=style_dim,
             latent_dim=latent_dim,
             num_domains=num_domains,
+            final_activation=final_activation,
         )
     latent_inference_model.load_checkpoint(checkpoint_iter)
     latent_inference_model.eval()
@@ -204,8 +210,11 @@ def get_counterfactual(
     best_idx_so_far = torch.argmax(p[:, target])
     if p[best_idx_so_far, target] > best_pred_so_far:
         best_pred_so_far = p[best_idx_so_far, target]
-        best_cf_so_far = xcf[best_idx_so_far].cpu().numpy()
-        best_cf_path_so_far = ref_paths[best_idx_so_far]
+        best_cf_so_far = xcf[best_idx_so_far].cpu()
+        if kind == "reference":
+            best_cf_path_so_far = ref_paths[best_idx_so_far]
+        else:
+            best_cf_path_so_far = None
     # Get the indices of the correct predictions
     indices = torch.where(predictions == target)[0]
 

src/quac/training/classification.py
@@ -13,7 +13,7 @@ class ClassifierWrapper(torch.nn.Module):
     for how to convert a model to torchscript.
     """
 
-    def __init__(self, model_checkpoint, mean: None, std: None):
+    def __init__(self, model_checkpoint, mean=None, std=None, assume_normalized=False):
         """Wraps a torchscript model, and applies normalization."""
         super().__init__()
         self.model = torch.jit.load(model_checkpoint)
@@ -21,11 +21,12 @@ class ClassifierWrapper(torch.nn.Module):
         self.transform = transforms.Normalize(mean, std)
         if mean is None:
             self.transform = Identity()
+        self.assume_normalized = assume_normalized
 
     def forward(self, x, assume_normalized=False):
         """Assumes that x is between -1 and 1."""
         # TODO it would be even better if the range was between 0 and 1 so we wouldn't have to do the below
-        if not assume_normalized:
+        if not self.assume_normalized and not assume_normalized:
             x = (x + 1) / 2
         x = self.transform(x)
         return self.model(x)