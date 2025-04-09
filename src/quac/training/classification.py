90c8420205197a13f1a1c47aef32ab66ea5c669d
Diane Adjavon <adjavond@hhmi.org>
2025-04-09 12:01:46 -0400

    refactor: :art: Simplify the classifier wrapper
    Swaps transforms for a simple scale and shift
    
    ClassifierWrapper no longer accepts the same parameters, which will have to be changed everywhere.
    Affects #64, closes #35, closes #33

--- src/quac/training/classification.py
+++ src/quac/training/classification.py
@@ -1,42 +1,37 @@
 import torch
-from torchvision import transforms
-
-
-class Identity(torch.nn.Module):
-    def forward(self, x):
-        return x
 
 
 class ClassifierWrapper(torch.nn.Module):
     """
-    This class expects a torchscript model. See [here](https://pytorch.org/tutorials/beginner/saving_loading_models.html#export-load-model-in-torchscript-format)
-    for how to convert a model to torchscript.
+    Wraps a torchscript model, and applies scale and shift in forward.
+
+    Parameters
+    ----------
+    model_checkpoint: str
+        Path to the torchscript model checkpoint.
+    scale: float
+        Scale factor. Defaults to 1. Applied to the data before passing it to the model.
+    shift: float
+        Shift factor. Defaults to 0. Applied to the data after the scale.
     """
 
     def __init__(
         self,
         model_checkpoint,
-        mean=None,
-        std=None,
-        assume_normalized=False,
-        do_nothing=False,
+        scale=1,
+        shift=0,
     ):
-        """Wraps a torchscript model, and applies normalization."""
         super().__init__()
         self.model = torch.jit.load(model_checkpoint)
         self.model.eval()
-        self.transform = transforms.Normalize(mean, std)
-        if mean is None:
-            self.transform = Identity()
-        self.assume_normalized = assume_normalized  # TODO Remove this, it's in forward
-        self.do_nothing = do_nothing
+        self.scale = scale
+        self.shift = shift
 
-    def forward(self, x, assume_normalized=False, do_nothing=False):
-        """Assumes that x is between -1 and 1."""
-        if do_nothing or self.do_nothing:
-            return self.model(x)
-        # TODO it would be even better if the range was between 0 and 1 so we wouldn't have to do the below
-        if not self.assume_normalized and not assume_normalized:
-            x = (x + 1) / 2
-        x = self.transform(x)
+    def forward(self, x):
+        """
+        Scales and shifts data, then runs it through the model.
+
+        Scale is applied first.
+        """
+        x = self.scale * x + self.shift
         return self.model(x)
